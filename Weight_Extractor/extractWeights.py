286347ba9f0170700a3f8cc492ca72bcb0e6791b
Miguel Reis <miguelareis98@gmail.com>
2022-03-22 15:20:23 +0000

    activations scale now visible, bias still not visible

--- Weight_Extractor/extractWeights.py
+++ Weight_Extractor/extractWeights.py
@@ -2,50 +2,53 @@ import argparse
 import os
 import numpy as np
 from tqdm import tqdm
+import torch
+import torch.nn as nn
+import torch.optim as optim
+
+import torchvision
 
 import brevitas.nn as qnn
 import math
 
 parser = argparse.ArgumentParser(description='Weights Extractor')
-parser.add_argument('--pth', default="./Example/example.pth", type=str, help='pth location')
+parser.add_argument('--pth', default="ckpt.pth", type=str, help='pth location')
 parser.add_argument('--weights', default=8, type=int, help='weights bit width')
 parser.add_argument('--activations', default=8, type=int, help='activations bit width')
-parser.add_argument('--folder', default="Example", type=str, help='activations bit width')
-parser.add_argument('--model', default="ResNet101Quant", type=str, help='activations bit width')
+parser.add_argument('--folder', default="Example", type=str, help='folder of the net')
+parser.add_argument('--modelFile', default="ResNet101Quant", type=str, help='name of net')
+parser.add_argument('--model', default="ResNet101Quant", type=str, help='name of net')
+parser.add_argument('--fullprint',action='store_true',help='resume from checkpoint')
 args = parser.parse_args()
 
-exec("from "+ args.folder + " import *")
-model = eval(args.model+"(weights=args.weights,activations=args.activations)")
-#model = ResNet101Quant(weights=args.weights,activations=args.activations)
-
-
-
+exec("from "+args.modelFile.replace(".py","",1)+" import *")
+eval(args.model+".setBitWidths(args.weights,args.activations)")
+model = eval(args.model+"()")
+model = torch.nn.DataParallel(model)
 state_dict = torch.load(args.pth, map_location='cpu')
-
-from collections import OrderedDict
-ckpt = OrderedDict()
-for k, v in state_dict["net"].items():
-    name = k.replace("module.","",1) # remove `module.`
-    ckpt[name] = v
-
-model.load_state_dict(ckpt,strict=True)
+model.load_state_dict(state_dict["net"])
 model.eval()
+model.to("cuda")
+
+if(args.fullprint):
+    torch.set_printoptions(profile="full")
 
 
-#model.eval()
-torch.set_printoptions(profile="full")
-
-for name, child in model.named_children():
-    for name_, param in child.named_parameters():
-        if "conv" in name or "conv" in name_:
-            if name_!="weight":
-                layerName="model."+name+'['+ name_.replace('.','].',1).replace("weight","int_weight()",1)
-            else:
-                layerName="model."+name+".int_weight()"
-            print(layerName)
-            tensors= eval(layerName)
-            scale=eval(layerName.replace("int_weight()","quant_weight_scale()",1))
-            scale=scale.detach().numpy()
-            print(tensors*scale)
-            print("Scale is "+str(scale)+ "\nFixed Point radix is "+ str(math.log(scale,2)) +" positions to the right")
-            input("Press Enter to continue...\n\n\n\n")
+for key, value in state_dict["net"].items():
+    if("weight" in key):
+        print(eval("model."+key.replace("weight","int_weight()",1)))
+        print("Layer Name = "+ key)
+        print("Scale = " + str(eval("model."+key.replace("weight","quant_weight_scale()",1))))
+        input("ENTER")
+    
+    if("bias" in key):
+        print(eval("model."+key))
+        print("Layer Name = "+ key)
+        #print("Scale = " + str(eval("model."+key.replace("bias","maybe_quant_bias_scale()",1))))
+        input("ENTER")
+    
+    if("relu" in key):
+        layerstr=key[:key.rfind("act_quant")-1]
+        print("Layer Name = "+ layerstr)
+        print("Scale = " + str(eval("model."+layerstr+".quant_act_scale()")))
+        input("ENTER")
